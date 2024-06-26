51db6720fa4675e65d255f7f0b9551be9b6d5f47
Jeongah19 <jeongah1919@gmail.com>
2024-06-27 00:15:24 +0900

    just test

---
+++ q2338.java
@@ -0,0 +1,13 @@
+import java.math.BigInteger;
+import java.util.*;
+public class q2338 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BigInteger A,B;
+        A=sc.nextBigInteger();
+        B=sc.nextBigInteger();
+        System.out.println(A.add(B));
+        System.out.println(A.subtract(B));
+        System.out.println(A.multiply(B));
+    }
+}
